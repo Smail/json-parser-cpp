092d53ac79fc11bda697a2ef151dfaab68f667ed
Smail <44433524+Smail@users.noreply.github.com>
2024-03-18 07:38:35 +0100

    parser: Implement boolean parsing

--- src/JSONParser.cpp
+++ src/JSONParser.cpp
@@ -16,4 +16,31 @@ auto parse_null(std::string_view json) -> std::optional<std::string_view> {
     auto cursor = json.begin();
     return parse_null(cursor, json.end());
 }
+
+auto parse_boolean(std::string_view::iterator& cursor, const std::string_view::iterator& end)
+    -> std::optional<bool> {
+    if (*cursor == 't') {
+        if ((cursor + 1) == end || *++cursor != 'r') return std::nullopt;
+        if ((cursor + 1) == end || *++cursor != 'u') return std::nullopt;
+        if ((cursor + 1) == end || *++cursor != 'e') return std::nullopt;
+
+        cursor += 1;
+        return true;
+    } else if (*cursor == 'f') {
+        if ((cursor + 1) == end || *++cursor != 'a') return std::nullopt;
+        if ((cursor + 1) == end || *++cursor != 'l') return std::nullopt;
+        if ((cursor + 1) == end || *++cursor != 's') return std::nullopt;
+        if ((cursor + 1) == end || *++cursor != 'e') return std::nullopt;
+
+        cursor += 1;
+        return false;
+    }
+
+    return std::nullopt;
+}
+
+auto parse_boolean(std::string_view json) -> std::optional<bool> {
+    auto cursor = json.begin();
+    return parse_boolean(cursor, json.end());
+}
 }  // namespace json
--- src/JSONParser.h
+++ src/JSONParser.h
@@ -9,4 +9,9 @@ namespace json {
     -> std::optional<std::string_view>;
 
 [[nodiscard]] auto parse_null(std::string_view json) -> std::optional<std::string_view>;
+
+[[nodiscard]] auto parse_boolean(std::string_view::iterator& cursor,
+                                 const std::string_view::iterator& end) -> std::optional<bool>;
+
+[[nodiscard]] auto parse_boolean(std::string_view json) -> std::optional<bool>;
 }  // namespace json
