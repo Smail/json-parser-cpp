9e989c3b17b73960befae447b74db0e99233bd75
Smail <44433524+Smail@users.noreply.github.com>
2024-03-18 07:27:07 +0100

    parser: Implement parsing of "null"

--- src/JSONParser.cpp
+++ src/JSONParser.cpp
@@ -1,3 +1,19 @@
 #include "JSONParser.h"
 
-namespace json {}  // namespace json
+namespace json {
+auto parse_null(std::string_view::iterator& cursor, const std::string_view::iterator& end)
+    -> std::optional<std::string_view> {
+    if (*cursor != 'n') return std::nullopt;
+    if ((cursor + 1) == end || *++cursor != 'u') return std::nullopt;
+    if ((cursor + 1) == end || *++cursor != 'l') return std::nullopt;
+    if ((cursor + 1) == end || *++cursor != 'l') return std::nullopt;
+
+    cursor += 1;
+    return "null";
+}
+
+auto parse_null(std::string_view json) -> std::optional<std::string_view> {
+    auto cursor = json.begin();
+    return parse_null(cursor, json.end());
+}
+}  // namespace json
--- src/JSONParser.h
+++ src/JSONParser.h
@@ -1,3 +1,12 @@
 #pragma once
 
-namespace json {}  // namespace json
+#include <optional>
+#include <string_view>
+
+namespace json {
+[[nodiscard]] auto parse_null(std::string_view::iterator& cursor,
+                              const std::string_view::iterator& end)
+    -> std::optional<std::string_view>;
+
+[[nodiscard]] auto parse_null(std::string_view json) -> std::optional<std::string_view>;
+}  // namespace json
