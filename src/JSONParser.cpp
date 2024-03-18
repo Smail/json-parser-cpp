74a15662f6c9c417243e9ea0db604b66ed996654
Smail <44433524+Smail@users.noreply.github.com>
2024-03-18 07:41:06 +0100

    parser: Implement number parsing

--- src/JSONParser.cpp
+++ src/JSONParser.cpp
@@ -1,5 +1,8 @@
 #include "JSONParser.h"
 
+#include <sstream>
+#include <string>
+
 namespace json {
 auto parse_null(std::string_view::iterator& cursor, const std::string_view::iterator& end)
     -> std::optional<std::string_view> {
@@ -42,5 +45,48 @@ auto parse_boolean(std::string_view::iterator& cursor, const std::string_view::i
 auto parse_boolean(std::string_view json) -> std::optional<bool> {
     auto cursor = json.begin();
     return parse_boolean(cursor, json.end());
+}
+
+auto parse_number(std::string_view::iterator& cursor, const std::string_view::iterator& end)
+    -> std::optional<double> {
+    std::stringstream ss{};
+
+    if (*cursor == '-' || *cursor == '+') ss << *cursor++;
+    if (!std::isdigit(*cursor)) return std::nullopt;
+
+    auto is_valid_number_char = [&cursor, &end]() {
+        char c = *cursor;
+        auto next = [&cursor](int offset) { return *(cursor + offset); };
+        auto has_next = [&cursor, &end](int offset) { return (cursor + offset) != end; };
+
+        if (std::isdigit(*cursor)) return true;
+        if (c == '.' && has_next(1) && std::isdigit(next(1))) return true;
+        // FIXME: Possible out of bounds if next(-1) is executed on first char, e.g. "+123"
+        if ((c == '+' || c == '-') && (next(-1) == 'e' || next(-1) == 'E')) return true;
+        if ((c == 'e' || c == 'E') && has_next(1)) {
+            char next_c = next(1);
+
+            if ((next_c == '+' || next_c == '-') && has_next(2)) {
+                next_c = next(2);
+            }
+
+            if (std::isdigit(next_c)) {
+                return true;
+            }
+        }
+
+        return false;
+    };
+
+    for (; cursor != end && is_valid_number_char(); ++cursor) {
+        ss << *cursor;
+    }
+
+    return std::stod(ss.str());
+}
+
+auto parse_number(std::string_view json) -> std::optional<double> {
+    auto cursor = json.begin();
+    return parse_number(cursor, json.end());
 }
 }  // namespace json
--- src/JSONParser.h
+++ src/JSONParser.h
@@ -14,4 +14,9 @@ namespace json {
                                  const std::string_view::iterator& end) -> std::optional<bool>;
 
 [[nodiscard]] auto parse_boolean(std::string_view json) -> std::optional<bool>;
+
+[[nodiscard]] auto parse_number(std::string_view::iterator& cursor,
+                                const std::string_view::iterator& end) -> std::optional<double>;
+
+[[nodiscard]] auto parse_number(std::string_view json) -> std::optional<double>;
 }  // namespace json
